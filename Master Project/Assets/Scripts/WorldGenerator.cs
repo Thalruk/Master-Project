e681fbc3a07ade6505095af4c06597db58764d20
Thalruk <Bartek.lipinski.pl@gmail.com>
2025-10-29 13:39:45 +0100

    First attempt

--- Master Project/Assets/Scripts/WorldGenerator.cs
+++ Master Project/Assets/Scripts/WorldGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
@@ -12,97 +13,150 @@ public class WorldGenerator : MonoBehaviour
     }
 
     [SerializeField] Blocks[,,] world;
-    [SerializeField] int XLength = 16;
-    [SerializeField] int YLength = 16;
-    [SerializeField] int Zlength = 16;
-
+    //TODO: change to vector3int later
+    [SerializeField] Vector3 Dimensions;
     [SerializeField] MeshFilter meshFilter;
     void Start()
     {
-        //GenerateWorld();
         GenerateMesh();
-        UpdateMesh();
     }
 
-    void Update()
+    private void GenerateData()
     {
-
-    }
-
-    private void GenerateWorld()
-    {
-        world = new Blocks[XLength, YLength, Zlength];
-        for (int x = 0; x < XLength; x++)
+        world = new Blocks[(int)Dimensions.x, (int)Dimensions.y, (int)Dimensions.z];
+        for (int x = 0; x < Dimensions.x; x++)
         {
-            for (int y = 0; y < YLength; y++)
+            for (int y = 0; y < Dimensions.y; y++)
             {
-                for (int z = 0; z < Zlength; z++)
+                for (int z = 0; z < Dimensions.z; z++)
                 {
-                    if (y < YLength / 2)
-                    {
-                        world[x, y, z] = Blocks.Dirt;
-                    }
-                    else
-                    {
-                        world[x, y, z] = Blocks.Air;
-                    }
+
+                    world[x, y, z] = GetBlock(new Vector3Int(x, y, z));
+                    //if (y < Dimensions.y / 2)
+                    //{
+                    //    world[x, y, z] = Blocks.Dirt;
+                    //}
+                    //else
+                    //{
+                    //    world[x, y, z] = Blocks.Air;
+                    //}
                 }
             }
         }
     }
 
-    Mesh mesh;
-    Vector3[] vertices;
-    int[] triangles;
     private void GenerateMesh()
     {
-        mesh = new Mesh();
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,1,0),
-            new Vector3(1,1,0),
-            new Vector3(1,0,0)
-        };
-        triangles = new int[]
-        {
-            0,1,2,
-            2,3,0
-        };
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
 
-    }
-
-    void UpdateMesh()
-    {
-        mesh.Clear();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
-        meshFilter.mesh = mesh;
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (world == null) return;
-        for (int x = 0; x < XLength; x++)
+        for (int x = 0; x < Dimensions.x; x++)
         {
-            for (int y = 0; y < YLength; y++)
+            for (int y = 0; y < Dimensions.y; y++)
             {
-                for (int z = 0; z < Zlength; z++)
+                for (int z = 0; z < Dimensions.z; z++)
                 {
-                    Gizmos.color = world[x, y, z] switch
+
+                    Vector3[] vertexPos = new Vector3[8]
                     {
-                        Blocks.Air => Color.cyan,
-                        Blocks.Grass => Color.green,
-                        Blocks.Dirt => new Color(0.545f, 0.271f, 0.075f),
-                        Blocks.Stone => Color.gray,
-                        Blocks.Bedrock => Color.black,
-                        _ => Color.magenta,
+                        new Vector3(-1,1,-1), new Vector3(-1,1,1),
+                        new Vector3(1,1,1), new Vector3(1,1,-1),
+                        new Vector3(-1,-1,-1), new Vector3(-1,-1,1),
+                        new Vector3(1,-1,-1), new Vector3(1,-1,1),
                     };
-                    Gizmos.DrawWireSphere(new Vector3(x, y, z), 0.5f);
+
+                    void AddQuad(int ai, int bi, int ci, int di, int i)
+                    {
+                        Vector3 a = vertexPos[ai];
+                        Vector3 b = vertexPos[bi];
+                        Vector3 c = vertexPos[ci];
+                        Vector3 d = vertexPos[di];
+                        vertices.AddRange(new List<Vector3> { a, b, c, d });
+                        triangles.AddRange(new List<int> { i, i + 1, i + 2, i, i + 2, i + 3 });
+                    }
+                    if (GetBlock(new Vector3Int(x, y, z)) != Blocks.Air)
+                    {
+                        if (GetBlock(new Vector3Int(x, y + 1, z)) == Blocks.Air)
+                        {
+                            //top
+                            AddQuad(0, 1, 2, 3, vertices.Count);
+                        }
+                        if (GetBlock(new Vector3Int(x, y - 1, z)) == Blocks.Air)
+                        {
+                            //bottom
+                            AddQuad(7, 6, 5, 4, vertices.Count);
+                        }
+
+                        if (GetBlock(new Vector3Int(x, y, z + 1)) == Blocks.Air)
+                        {
+                            //front
+                            AddQuad(3, 2, 6, 7, vertices.Count);
+                        }
+                        if (GetBlock(new Vector3Int(x, y, z - 1)) == Blocks.Air)
+                        {
+                            //back
+                            AddQuad(1, 0, 4, 5, vertices.Count);
+                        }
+
+                        if (GetBlock(new Vector3Int(x + 1, y, z)) == Blocks.Air)
+                        {
+                            //right
+                            AddQuad(2, 1, 5, 6, vertices.Count);
+                        }
+                        if (GetBlock(new Vector3Int(x - 1, y, z)) == Blocks.Air)
+                        {
+                            //left
+                            AddQuad(0, 3, 7, 4, vertices.Count);
+                        }
+                    }
                 }
             }
+        }
+
+        meshFilter.mesh = new Mesh()
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray()
+        };
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+
+
+    //private void OnDrawGizmos()
+    //{
+    //    if (world == null) return;
+    //    for (int x = 0; x < Dimensions.x; x++)
+    //    {
+    //        for (int y = 0; y < Dimensions.y; y++)
+    //        {
+    //            for (int z = 0; z < Dimensions.z; z++)
+    //            {
+    //                Gizmos.color = world[x, y, z] switch
+    //                {
+    //                    Blocks.Air => Color.cyan,
+    //                    Blocks.Grass => Color.green,
+    //                    Blocks.Dirt => new Color(0.545f, 0.271f, 0.075f),
+    //                    Blocks.Stone => Color.gray,
+    //                    Blocks.Bedrock => Color.black,
+    //                    _ => Color.magenta,
+    //                };
+    //                Gizmos.DrawWireSphere(new Vector3(x, y, z), 0.5f);
+    //            }
+    //        }
+    //    }
+
+    //}
+    Blocks GetBlock(Vector3Int coordinates)
+    {
+        if (Mathf.PerlinNoise(coordinates.x * 0.1f, coordinates.y * 0.1f) > 0.5f)
+        {
+            return Blocks.Dirt;
+        }
+        else
+        {
+            return Blocks.Air;
         }
     }
 }
